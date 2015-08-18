a2637085eef570149e5f9f4fd025879d63adf762
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2015-08-18 23:11:01 +0200

    Data i czas wschodu i zachodu pokrywają się mniej więcej z rzeczywistością

--- DaylightModel/SunPosition.cpp
+++ DaylightModel/SunPosition.cpp
@@ -44,13 +44,13 @@ double SunPosition::computeStandardMeridian( double longitude )
 
 double SunPosition::computeAzimuth()
 {
-	double translateFun = -3 * M_PI;
+	double translateFun = -M_PI;
 	return atan2( -cos( m_solarDeclination ) * sin( M_PI * m_daytime / 12.0 - translateFun ), ( cos( m_latitude ) * sin( m_solarDeclination ) - sin( m_latitude ) * cos( m_solarDeclination ) * cos( M_PI * m_daytime / 12.0 - translateFun ) ) );
 }
 
 double SunPosition::computeElevation()
 {
-	return /*M_PI / 2 - */asin( sin( m_latitude ) * sin( m_solarDeclination ) - cos( m_latitude ) * cos( m_solarDeclination ) * cos( M_PI * m_daytime / 12.0 ) );
+	return asin( sin( m_latitude ) * sin( m_solarDeclination ) - cos( m_latitude ) * cos( m_solarDeclination ) * cos( M_PI * m_daytime / 12.0 ) );
 }
 
 double SunPosition::setSunConditions( float latit, float longit, tm* time )
@@ -61,14 +61,14 @@ double SunPosition::setSunConditions( float latit, float longit, tm* time )
 	m_longitude = static_cast<float>( longit *  M_PI / 180 );
 
 	// Czas standardowy w godzinach
-	double standardMeridian = computeStandardMeridian( m_longitude );
+	double standardMeridian = computeStandardMeridian( longit ) *  M_PI / 180;
 	double standard_time = time->tm_hour + time->tm_min / 60.0 + time->tm_sec / 3600.0;
 	double julian_day = time->tm_yday + 1;
-	double solar_time = standard_time + 0.17*sin( 4* M_PI * ( julian_day - 80 ) / 373.0 );
+	double solar_time = standard_time + 0.17*sin( 4 * M_PI * ( julian_day - 80 ) / 373.0 );
 	solar_time -= 0.129 * ( 2 * M_PI * ( julian_day - 8 ) / 355 );
-	//solar_time += 12 * ( standardMeridian - m_longitude ) / M_PI;
+	solar_time += 12 * ( standardMeridian - m_longitude ) / M_PI;
 
-	m_solarDeclination = -0.4093 * sin( 2 * M_PI * ( julian_day - 81 ) / 365.0 );
+	m_solarDeclination = 0.4093 * sin( 2 * M_PI * ( julian_day - 81 ) / 365.0 );
 
  	setSunConditions( solar_time );
 	return solar_time;
